50320c9dd4ba971bb30e14c282193799996b9ec3
ismail <icb_1998@hotmail.com>
2023-12-04 02:52:18 +0300

    EDİTProje dosyası ekle.

---
+++ Core/RentACar.Application/DTOs/ServiceDTO.cs
@@ -0,0 +1,14 @@
+﻿using RentACar.Application.DTOs.BaseModelDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.DTOs
+{
+    public class ServiceDTO:BaseModelDTO
+    {
+        public string ServiceName { get; set; }
+    }
+}
