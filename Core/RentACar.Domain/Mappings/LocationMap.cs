e581183ec58ad501e66eba5260e1ed4735108020
MEHMET GÜÇ <mehmetquc@gmail.com>
2023-12-06 02:08:25 +0300

    edit maping2

---
+++ Core/RentACar.Domain/Mappings/LocationMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Domain.Mappings
+{
+    public class LocationMap : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Location> builder)
+        {
+            
+        }
+    }
+}
