3193bdf2ee1aee270f64df3588d1925fc56ef68f
ismail <icb_1998@hotmail.com>
2023-12-09 03:53:51 +0300

    #1 iki count service concrate and abstract created

---
+++ Core/RentACar.Application/IServices/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using RentACar.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.IServices
+{
+    public interface ICarService
+    {
+        public Task<List<CarDTO>> GetCars();
+        public Task<CarDTO> CreateCar(CarDTO Car);
+        public Task<CarDTO> UpdateCar(CarDTO car);
+        public Task<bool> DeleteCarId(Guid id);
+        public Task<CarDTO> GetCarById(Guid Id);
+    }
+}
---
+++ Infrastructure/RentACar.Persistence/Services/CarService.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RentACar.Application.DTOs;
+using RentACar.Application.IServices;
+using RentACar.Domain.Models;
+using RentACar.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly IMapper mapper;
+        private readonly IConfiguration configuration;
+        private readonly RentACarPsqlDbContext context;
+        public CarService(IMapper _mapper,IConfiguration _configuration,RentACarPsqlDbContext _context)
+        {
+            mapper= _mapper;
+            configuration=_configuration;
+            context= _context;
+        }
+        public async Task<CarDTO> CreateCar(CarDTO Car)
+        {
+           var dbCar = await context.Cars.Where(c => c.Id == Car.Id).FirstOrDefaultAsync();
+            if (dbCar != null)
+                throw new Exception("Bu Araba Zaten Sistemde Kayıtlı");
+            dbCar = mapper.Map<Car>(Car);
+            dbCar.CreateDate = DateTime.UtcNow;
+            await context.Cars.AddAsync(dbCar);
+            int result = await context.SaveChangesAsync();
+
+            return mapper.Map<CarDTO>(dbCar);
+            
+        }
+
+        public async Task<bool> DeleteCarId(Guid id)
+        {
+            var dbCar =await context.Cars.Where(c=>c.Id==id).FirstOrDefaultAsync();
+            if (dbCar == null)
+                throw new Exception("Araba Bulunamadı");
+            context.Cars.Remove(dbCar);
+            int result= await context.SaveChangesAsync();
+            return result > 0;
+        }
+
+        public async Task<CarDTO> GetCarById(Guid Id)
+        {
+            var dbCar= await context.Cars.Where(c => c.Id == Id)
+                .ProjectTo<CarDTO>(mapper.ConfigurationProvider).FirstOrDefaultAsync();
+            if (dbCar == null)
+                throw new Exception("Araba Bulunamadı.");
+            return dbCar;
+        }
+
+        public async Task<List<CarDTO>> GetCars()
+        {
+            var dbCar = await context.Cars.ProjectTo<CarDTO>(mapper.ConfigurationProvider).ToListAsync();
+            return dbCar;
+        }
+
+        public async Task<CarDTO> UpdateCar(CarDTO car)
+        {
+            var dbCar = await context.Cars.Where(c => c.Id == car.Id).FirstOrDefaultAsync();
+            if (dbCar == null)
+                throw new Exception("Araba Bulunamadığından Dolayı Güncelleme İşlemi Başarısız");
+            mapper.Map(car, dbCar);
+
+            int result=await context.SaveChangesAsync();
+            return mapper.Map<CarDTO>(dbCar);
+        }
+    }
+}
