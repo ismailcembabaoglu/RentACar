7c0f0366534d2c0bf06291668b3800c77c372ea9
MEHMET GÜÇ <mehmetquc@gmail.com>
2023-12-18 03:18:44 +0300

    Car CarLocation Location pages admin paneli  eklendi

--- Core/RentACar.Application/Validators/CarValidator.cs
+++ Core/RentACar.Application/Validators/CarValidator.cs
@@ -15,6 +15,15 @@ namespace RentACar.Application.Validators
             RuleFor(c=>c.CarName).NotEmpty().WithMessage("Araba Adı Boş Geçilemez!!");
             RuleFor(c => c.CarModel).NotEmpty().WithMessage("Araba Modeli Boş Geçilemez!!");
             RuleFor(c => c.Photo).NotEmpty().WithMessage("Fotoğraf Alanı Boş Geçilemez!!");
+            RuleFor(c => c.Person).NotEmpty().WithMessage("Kişi Alanı Boş Geçilemez!!");
+            RuleFor(c => c.Luggage).NotEmpty().WithMessage("Bağaj Alanı Boş Geçilemez!!");
+            RuleFor(c => c.Door).NotEmpty().WithMessage("Kapı Alanı Boş Geçilemez!!");
+            RuleFor(c => c.gear).NotEmpty().WithMessage("Vites Alanı Boş Geçilemez!!");
+            RuleFor(c => c.FuelType).NotEmpty().WithMessage("Yakıt Tipi Alanı Boş Geçilemez!!");
+            RuleFor(c => c.CarYear).NotEmpty().WithMessage("Araba Yıl Alanı Boş Geçilemez!!");
+            RuleFor(c => c.Depozit).NotEmpty().WithMessage("Depozito Alanı Boş Geçilemez!!");
+            RuleFor(c => c.TotalKm).NotEmpty().WithMessage("Total Km Alanı Boş Geçilemez!!");
+            
         }
     }
 }
---
+++ Core/RentACar.Application/Validators/ReservationValitadar.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using RentACar.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Validators
+{
+    public class ReservationValitadar : AbstractValidator<ReservationDTO>
+    {
+        public ReservationValitadar() 
+        {
+            RuleFor(c => c.RentPrice).NotEmpty().WithMessage("Kiralık Fiyat Boş Geçilemez!!");
+            RuleFor(c => c.TotalPrice).NotEmpty().WithMessage("Toplam Fiyat Boş Geçilemez!!");
+
+        }   
+
+    }
+}
