452af411d3b9d6c62d8135570e3ce361683b8c6b
ismailcembabaoglu <icb_1998@hotmail.com>
2024-01-16 09:22:19 +0300

    update rezervation

--- Core/RentACar.Application/Validators/ReservationValitador.cs
+++ Core/RentACar.Application/Validators/ReservationValitador.cs
@@ -18,6 +18,7 @@ namespace RentACar.Application.Validators
             RuleFor(c => c.Address).NotEmpty().WithMessage("Adres Boş Geçilemez!!");
             RuleFor(c => c.CustomerName).NotEmpty().WithMessage("Şirket Veya İsim Boş Geçilemez!!");
             RuleFor(c => c.IdentityNo).NotEmpty().WithMessage("Kimlik No Veya Pasaport Boş Geçilemez!!");
+            RuleFor(c => c.PhoneNumber).NotEmpty().WithMessage("Telefon Numarası Boş Geçilemez!!").MinimumLength(10).WithMessage("Telefon Numarası En Az 10 Olmalı");
             RuleFor(c => c.Email).NotEmpty().WithMessage("Email Boş Geçilemez!!").EmailAddress().WithMessage("Mail Adresi Yanlış!!");
 
         }   
